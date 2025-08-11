777ef41c25b9a87401f4e6f0d9da4a2196ec1cd4
nift4 <nift4@protonmail.com>
2025-08-11 14:48:20 +0200

    Drop stale events to fix race when device info changes

--- libraries/session/src/main/java/androidx/media3/session/MediaSessionLegacyStub.java
+++ libraries/session/src/main/java/androidx/media3/session/MediaSessionLegacyStub.java
@@ -139,13 +139,13 @@ import org.checkerframework.checker.initialization.qual.Initialized;
   private final MediaSessionCompat sessionCompat;
   @Nullable private final MediaButtonReceiver runtimeBroadcastReceiver;
   @Nullable private final ComponentName broadcastReceiverComponentName;
-  @Nullable private VolumeProviderCompat volumeProviderCompat;
   private final boolean playIfSuppressed;
   private final HandlerThread compatSessionInteractionThread;
   private final Handler compatSessionInteractionHandler;
 
   private volatile long connectionTimeoutMs;
   @Nullable private FutureCallback<Bitmap> pendingBitmapLoadCallback;
+  @Nullable private VolumeProviderCompat volumeProviderCompat;
   private int sessionFlags;
   @Nullable private LegacyError legacyError;
   private Bundle legacyExtras;
@@ -1674,6 +1674,10 @@ import org.checkerframework.checker.initialization.qual.Initialized;
       if (playbackType == DeviceInfo.PLAYBACK_TYPE_LOCAL) {
         postOrRunForCompatSession(
             () -> {
+              if (volumeProviderCompat != null) {
+                // Stale event.
+                return;
+              }
               sessionCompat.setPlaybackToLocal(audioAttributes.getStreamType());
               sessionImpl.onNotificationRefreshRequired();
             });
@@ -1687,11 +1691,19 @@ import org.checkerframework.checker.initialization.qual.Initialized;
       if (volumeProviderCompat == null) {
         int streamType = player.getAudioAttributesWithCommandCheck().getStreamType();
         postOrRunForCompatSession(() -> {
+          if (volumeProviderCompat != null) {
+            // Stale event.
+            return;
+          }
           sessionCompat.setPlaybackToLocal(streamType);
           sessionImpl.onNotificationRefreshRequired();
         });
       } else {
         postOrRunForCompatSession(() -> {
+          if (volumeProviderCompat == null) {
+            // Stale event.
+            return;
+          }
           sessionCompat.setPlaybackToRemote(volumeProviderCompat);
           sessionImpl.onNotificationRefreshRequired();
         });
